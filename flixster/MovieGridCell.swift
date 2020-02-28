6c75bd3a666e6097a517aa327dc969a6a313356f
Caitlyn Chen <cate.kat16@gmail.com>
2020-02-28 02:28:54 -0500

    First commit

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Xiaoyan Yang on 2/28/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
